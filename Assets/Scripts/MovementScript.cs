158935e006ff0e9716b12963bea3e2b7b1a8f675
Josep de Cid <josep.de.cid@gmail.com>
2017-06-27 14:03:58 +0200

    Add new movement script

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -3,98 +3,135 @@ using System.Collections;
 
 public class MovementScript : MonoBehaviour
 {
-    public float deltaMovement = 2f;
-	public float speed = 10.0f;
-	public float gravity = 10.0f;
-	public float maxVelocityChange = 10.0f;
-	public bool canJump = true;
-	public float jumpHeight = 1.0f;
-	private bool grounded = false;
-	Rigidbody rb;
+    public float walkSpeed = 6.0f;
+    public float runSpeed = 11.0f;
 
-    void Start ()
+    public bool limitDiagonalSpeed = true;
+    public bool toggleRun = false;
+
+    public float jumpSpeed = 8.0f;
+    public float gravity = 9.8f;
+
+    public bool slideWhenOverSlopeLimit = false;
+    public bool slideOnTaggedObjects = false;
+    public float slideSpeed = 12.0f;
+
+    public bool airControl = false;
+    public float antiBumpFactor = 0.75f;
+    public int antiBunnyHopFactor = 1;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private bool grounded = false;
+    private CharacterController controller;
+    private Transform myTransform;
+    private float speed;
+    private RaycastHit hit;
+    private float fallStartLevel;
+    private bool falling;
+    private float slideLimit;
+    private float rayDistance;
+    private Vector3 contactPoint;
+    private bool playerControl = false;
+    private int jumpTimer;
+
+    void Start()
     {
-
+        controller = GetComponent<CharacterController>();
+        myTransform = transform;
+        speed = walkSpeed;
+        rayDistance = controller.height * .5f + controller.radius;
+        slideLimit = controller.slopeLimit - .1f;
+        jumpTimer = antiBunnyHopFactor;
     }
 
-    void Update ()
+    void FixedUpdate()
     {
-        Movement();
+        bool W = Input.GetKey(KeyCode.W);
+        bool A = Input.GetKey(KeyCode.A);
+        bool S = !W && Input.GetKey(KeyCode.S);
+        bool D = !A && Input.GetKey(KeyCode.D);
+
+        float inputX = (W || S) ? (W ? 1.0f : -1.0f) : 0f;
+        float inputY = (A || D) ? (D ? 1.0f : -1.0f) : 0f;
+
+        float inputModifyFactor = ((W || S) && (A || D) && limitDiagonalSpeed) ? .7071f : 1.0f;
+
+        if (grounded)
+        {
+            bool sliding = false;
+
+            if (Physics.Raycast(myTransform.position, -Vector3.up, out hit, rayDistance))
+            {
+                if (Vector3.Angle(hit.normal, Vector3.up) > slideLimit)
+                {
+                    sliding = true;
+                }
+            }
+            else
+            {
+                Physics.Raycast(contactPoint + Vector3.up, -Vector3.up, out hit);
+                if (Vector3.Angle(hit.normal, Vector3.up) > slideLimit) {
+                    sliding = true;
+                }
+            }
+
+            if (!toggleRun)
+            {
+                speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+            }
+
+            if ((sliding && slideWhenOverSlopeLimit) || (slideOnTaggedObjects && hit.collider.tag == "Slide"))
+            {
+                Vector3 hitNormal = hit.normal;
+                moveDirection = new Vector3(hitNormal.x, -hitNormal.y, hitNormal.z);
+                Vector3.OrthoNormalize(ref hitNormal, ref moveDirection);
+                moveDirection *= slideSpeed;
+                playerControl = false;
+            }
+            else
+            {
+                moveDirection = new Vector3(inputX * inputModifyFactor, -antiBumpFactor, inputY * inputModifyFactor);
+                moveDirection = new Vector3(1.0f * inputModifyFactor, -antiBumpFactor, 1.0f * inputModifyFactor);
+                moveDirection = myTransform.TransformDirection(moveDirection) * speed;
+                playerControl = true;
+            }
+
+            if (!Input.GetKey(KeyCode.Space))
+            {
+                ++jumpTimer;
+            }
+            else if (jumpTimer >= antiBunnyHopFactor)
+            {
+                moveDirection.y = jumpSpeed;
+                jumpTimer = 0;
+            }
+        }
+        else
+        {
+            if (airControl && playerControl)
+            {
+                moveDirection.x = inputX * speed * inputModifyFactor;
+                moveDirection.z = inputY * speed * inputModifyFactor;
+                moveDirection = myTransform.TransformDirection(moveDirection);
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        grounded = (controller.Move(moveDirection * Time.deltaTime) & CollisionFlags.Below) != 0;
     }
 
-    void OnCollisionStay (Collision collision)
+    void Update()
     {
-        if (collision.gameObject.name == "Plane_base")
-            if (Input.GetKeyDown(KeyCode.Space))
-                GetComponent<Rigidbody>().AddForce(Vector3.up * 50f, ForceMode.Impulse);
-    }
-    
-    private void Movement ()
-    {
-        if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector3.forward * deltaMovement * Time.deltaTime);
-        if (Input.GetKey(KeyCode.S))
-            transform.Translate(Vector3.back * deltaMovement * Time.deltaTime);
-        if (Input.GetKey(KeyCode.A))
-            transform.Translate(Vector3.left * deltaMovement * Time.deltaTime);
-        if (Input.GetKey(KeyCode.D))
-            transform.Translate(Vector3.right * deltaMovement * Time.deltaTime);
+        // If the run button is set to toggle, then switch between walk/run speed. (We use Update for this...
+        // FixedUpdate is a poor place to use GetButtonDown, since it doesn't necessarily run every frame and can miss the event)
+        if (toggleRun && grounded && Input.GetButtonDown("Run"))
+            speed = (speed == walkSpeed ? runSpeed : walkSpeed);
     }
 
-/*    private void RecollectAmmo ()
+    // Store point that we're in contact with for use in FixedUpdate if needed
+    void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        _shoot.AddAmmo();
-        GameObject usedWeapon = GameObject.Find("Weapon_pp7");
-        Destroy(usedWeapon);
-    }*/
-
-	void Awake()
-	{
-		rb = gameObject.GetComponent<Rigidbody>();
-		rb.freezeRotation = true;
-		rb.useGravity = false;
-	}
-
-	void FixedUpdate()
-	{
-		if (grounded)
-		{
-			Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			targetVelocity = transform.TransformDirection(targetVelocity);
-			targetVelocity *= speed;
-
-			// Apply a force that attempts to reach our target velocity
-			Vector3 velocity = rb.velocity;
-			Vector3 velocityChange = (targetVelocity - velocity);
-			velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-			velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
-			velocityChange.y = 0;
-			rb.AddForce(velocityChange, ForceMode.VelocityChange);
-
-			// Jump
-			if (canJump && Input.GetButton("Jump"))
-			{
-				rb.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
-			}
-		}
-
-		rb.AddForce(new Vector3(0, -gravity * rb.mass, 0));
-
-		grounded = false;
-	}
-
-	void OnCollisionStay()
-	{
-		grounded = true;
-	}
-
-	float CalculateJumpVerticalSpeed()
-	{
-		return Mathf.Sqrt(2 * jumpHeight * gravity);
-	}
-
-	public void ModifySpeed(float difference)
-	{
-		speed += difference;
-	}
+        contactPoint = hit.point;
+    }
 }
