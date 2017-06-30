ed822f5359b246bbcf0d860bfa9891fecdb05ab2
Josep de Cid <josep.de.cid@gmail.com>
2017-06-30 11:21:02 +0200

    Implement basic AI

---
+++ Assets/Scripts/IASoldiers.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class IASoldiers : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+	}
+}
