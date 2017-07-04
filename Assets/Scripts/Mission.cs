f7b287442c2c15be318e8682fdde14ae1ed1d4e2
JosepRIvaille <josep.de.cid@gmail.com>
2017-07-04 03:11:58 +0200

    Add missions structure

--- Assets/Scripts/Mission.cs
+++ Assets/Scripts/Mission.cs
@@ -4,13 +4,62 @@ using UnityEngine;
 
 public class Mission : MonoBehaviour {
 
-	// Use this for initialization
+	private List<Objective> objectives;
+
 	void Start () {
-		
+		objectives = new List<> ();
+		int i = 0;
+		while (PlayerPrefs.HasKey("Mission" + i.ToString))
+		{
+			objectives.Add(PlayerPrefs.GetString ("Mission" + i.toString));
+			++i;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void UpdateProgress(int objectiveId)
+	{
+		foreach (Objective o in objectives)
+		{
+			if (o.GetId() == objectiveId)
+			{
+				if (o.UpdateProgress ())
+				{
+					Debug.Log ("Mission acomplished");
+				}
+			}
+		}
+	}
+
+	private class Objective
+	{
+		private int id;
+		private string description;
+		private bool completed;
+		private int progress;
+		private int goal;
+
+		public Objective(int id, string desctiption, int goal)
+		{
+			this.id = id;
+			this.description = description;
+			this.completed = false;
+			this.goal = goal;
+		}
+
+		public int GetId()
+		{
+			return this.id;
+		}
+
+		public bool UpdateProgress()
+		{
+			++(this.progress);
+			return CheckCompleted();
+		}
+
+		private void CheckCompleted()
+		{
+			this.completed = this.goal == this.progress;
+		}
 	}
 }
