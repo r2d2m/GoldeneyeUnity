b994925c1bc243b75cb6320a5d8eb4b043ae04cb
JosepRIvaille <josep.de.cid@gmail.com>
2017-07-07 01:41:34 +0200

    Fix shooting IA problems

--- Assets/Scripts/IASoldiers.cs
+++ Assets/Scripts/IASoldiers.cs
@@ -9,29 +9,33 @@ public class IASoldiers : MonoBehaviour {
     public float seenDistance = 15f;
     public float helpDistance = 50f;
     public float shootDistance = 15f;
-    public int consecutiveShoots = 1000;
+    public int consecutiveShoots = 14;
 
 	public bool activatesAlarm = false;
     public GameObject muzzle;
 
     public AudioClip[] reactions;
-    private AudioSource source;
+	private AudioSource source, weaponSource, jamesSound;
     private Animator animator;
     private Rigidbody rb;
 
     public Transform target;
 	public GameObject alarm;
+	public GameObject weapon;
 
     NavMeshAgent agent;
     IASoldiers _iaSoldiers;
 
     private bool canShoot = true;
     private bool isHostile = false;
+	private bool locked = false;
     private int currentShoots = 0;
      
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
         source = GetComponent<AudioSource>();
+		weaponSource = weapon.GetComponent<AudioSource>();
+		jamesSound = GameObject.Find("007").GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
 	}
@@ -55,7 +59,7 @@ public class IASoldiers : MonoBehaviour {
 				}
             }
         }
-        else
+		else if (!locked || (!animator.GetCurrentAnimatorStateInfo(0).IsName("Reload") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Shoot")))
         {
 			if (activatesAlarm && alarm != null && !alarm.GetComponent<AudioSource>().isPlaying) {
 				agent.SetDestination(alarm.transform.position);
@@ -74,14 +78,14 @@ public class IASoldiers : MonoBehaviour {
                 if (currentShoots == consecutiveShoots)
                 {
                     animator.SetBool("Shooting", false);
+					locked = true;
+					canShoot = true;
                 }
                 else if (canShoot)
                 {
-                    ++currentShoots;
-                    canShoot = false;
-                    animator.SetBool("Shooting", true);
-                    transform.LookAt(target.transform, Vector3.up);
-					muzzle.GetComponent<ParticleSystem>().Play();
+					canShoot = false;
+					animator.SetBool("Shooting", true);
+					transform.LookAt(target.transform, Vector3.up);
                 }
             }
             animator.SetBool("Moving", rb.velocity.magnitude > 0f);
@@ -107,10 +111,23 @@ public class IASoldiers : MonoBehaviour {
     public void EventReloadBullets()
     {
         currentShoots = 0;
+		animator.SetInteger("Bullets", consecutiveShoots);
+		locked = false;
     }
 
     public void EventCanShootAgain()
     {
         canShoot = true;
     }
+
+	public void EventShootMuzzle()
+	{
+		if (!weaponSource.isPlaying)
+		{
+			++currentShoots;
+			weaponSource.Play();
+			jamesSound.Play();
+			muzzle.GetComponent<ParticleSystem>().Play();
+		}
+	}
 }
