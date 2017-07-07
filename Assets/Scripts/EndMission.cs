e63d252cde4b8c30e27ff18fba1dce25d47cb3bc
JosepRIvaille <josep.de.cid@gmail.com>
2017-07-07 02:36:19 +0200

    Add end mission script

---
+++ Assets/Scripts/EndMission.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EndMission : MonoBehaviour {
+
+	private bool finishing = false;
+	private Camera camera;
+
+	void Start()
+	{
+		camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+	}
+
+	void Update()
+	{
+		if (finishing)
+		{
+			camera.fieldOfView -= Time.deltaTime * 50;
+		}
+		if (camera.fieldOfView <= 0)
+		{
+			finishing = false;
+			SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Single);
+		}
+	}
+
+	void OnTriggerEnter(Collider hit)
+	{
+		if (hit.gameObject.tag == "Player")
+		{
+			finishing = true;
+		}
+	}
+}
